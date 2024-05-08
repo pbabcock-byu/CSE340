626a6aeee108a46f1ea91d35b38da08f1f192cf9
Peter Babcock <peterb@saefl.com>
2024-05-07 21:58:09 -0700

    week3 rebuild

--- server.js
+++ server.js
@@ -23,12 +23,7 @@ app.set("layout", "./layouts/layout") // not at views root
  * Routes
  *************************/
 app.use(static)
-
-/* Index route
-app.get("/", function(req, res){
-  res.render("index", {title: "Home"})
-})
-*/
+// Index route
 app.get("/", baseController.buildHome)
 
 /* ***********************
